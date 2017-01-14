1a2bc12cd5ffbc4f601999bab87114335e941f2e
cKeinsley <keinsleyChris@gmail.com>
2017-01-14 17:12:48 -0500

    Added button listeners and updated some UI -Christopher Keinsley

--- app/src/main/java/edu/rosehulman/keinslc/rhithmu/MainActivity.java
+++ app/src/main/java/edu/rosehulman/keinslc/rhithmu/MainActivity.java
@@ -35,39 +35,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Grab references
+        // Capture references
         mWeekView = (WeekView) findViewById(R.id.weekView);
         mMatchScheduleButton = (Button) findViewById(R.id.matchSyncSchedule);
+        // Day Buttons
         mTodayButton = (Button) findViewById(R.id.todayButton);
         mOneDayButton = (Button) findViewById(R.id.oneDayButton);
         mThreeDayButton = (Button) findViewById(R.id.threeDayButton);
         mWeekButton = (Button) findViewById(R.id.sevenDayButton);
+        setButtonListeners();
 
-//        setDayButtonListeners();
-
-    // Set an action when any event is clicked.
+        // Set an action when any event is clicked.
         mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
             @Override
             public void onEventClick(WeekViewEvent event, RectF eventRect) {
                 Log.d("MAIN", "On Event Clicked");
+                // TODO generate alert dialog with description of event
             }
         });
 
-    // The week view has infinite scrolling horizontally. We have to provide the events of a
-    // month every time the month changes on the week view.
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
         mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
             @Override
             public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
                 Log.d("MAIN", "Month Changed");
-                return new ArrayList<WeekViewEvent>();
+                // TODO generate the events in the given month/year
+                ArrayList<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+                events.add(new WeekViewEvent(5, "Test", 2017, 1, 14, 6, 25, 2017, 1, 14, 8, 5));
+                return events;
             }
         });
 
-    // Set long press listener for events.
+        // Set long press listener for events.
         mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
             @Override
             public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
                 Log.d("MAIN", "On Event Clicked Long Press");
+                // TODO Load information about event and launch add/edit event activity
             }
         });
 
@@ -76,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null);
             }
         });
     }
@@ -96,10 +101,50 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case (R.id.action_settings):
+                Log.d("MAIN", "Settings Pressed");
+                return true;
+            case (R.id.action_importClasses):
+                Log.d("MAIN", "Import Classes Pressed");
+                return true;
         }
 
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setButtonListeners() {
+        mMatchScheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Launch a bluetooth connect fragment
+                Log.d("MAIN", "Match Schedules Clicked");
+            }
+        });
+        mTodayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWeekView.goToToday();
+            }
+        });
+        mOneDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWeekView.setNumberOfVisibleDays(1);
+            }
+        });
+        mThreeDayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWeekView.setNumberOfVisibleDays(3);
+            }
+        });
+        mWeekButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWeekView.setNumberOfVisibleDays(7);
+            }
+        });
     }
 }
