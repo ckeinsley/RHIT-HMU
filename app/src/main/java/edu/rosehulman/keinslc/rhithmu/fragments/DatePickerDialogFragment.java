a6461512159d787c2c03ffad1ed975a354a53b4d
DavisGilmore <gilmordw@GILMORDW-W541.rose-hulman.edu>
2017-01-15 16:42:19 -0500

    made onCreateDialog methods in fragments - Davis

--- app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/DatePickerDialogFragment.java
+++ app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/DatePickerDialogFragment.java
@@ -1,10 +1,44 @@
 package edu.rosehulman.keinslc.rhithmu.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import edu.rosehulman.keinslc.rhithmu.R;
 
 /**
  * Created by gilmordw on 1/15/2017.
  */
 
 public class DatePickerDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(
+                R.layout.dialog_fragment_date_picker, null);
+        builder.setView(view);
+
+        // TODO: Capture views and grab mEvent from bundle
+        final DatePicker mDatePicker = (DatePicker) view.findViewById(R.id.date_picker);
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("FRAG", "OK Clicked");
+
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create();
+    }
 }
--- app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/TimePickerDialogFragment.java
+++ app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/TimePickerDialogFragment.java
@@ -1,10 +1,42 @@
 package edu.rosehulman.keinslc.rhithmu.fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
+import edu.rosehulman.keinslc.rhithmu.R;
 
 /**
  * Created by gilmordw on 1/15/2017.
  */
 
 public class TimePickerDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(
+                R.layout.dialog_fragment_time_picker, null);
+        builder.setView(view);
+
+        // TODO: Capture views and grab mEvent from bundle
+        final TimePicker mTimePicker = (TimePicker) view.findViewById(R.id.time_picker);
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("FRAG", "OK Clicked");
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        return builder.create();
+    }
 }
