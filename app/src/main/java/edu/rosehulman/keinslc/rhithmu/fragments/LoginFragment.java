bbdf5654fa3b5977f4e8a1cc4e288b4c51f57907
cKeinsley <keinsleyChris@gmail.com>
2017-02-08 10:53:38 -0500

    removed email/password authentication option as we won't be supporting account creation

--- app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/LoginFragment.java
+++ app/src/main/java/edu/rosehulman/keinslc/rhithmu/fragments/LoginFragment.java
@@ -4,15 +4,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.common.SignInButton;
 
@@ -24,13 +18,11 @@ import edu.rosehulman.keinslc.rhithmu.R;
  */
 public class LoginFragment extends Fragment {
 
-    private EditText mPasswordView;
-    private EditText mEmailView;
-    private View mLoginForm;
     private View mProgressSpinner;
     private boolean mLoggingIn;
     private OnLoginListener mListener;
     private SignInButton mGoogleSignInButton;
+    private View rosefireLoginButton;
 
     public LoginFragment() {
     }
@@ -45,39 +37,10 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-        mEmailView = (EditText) rootView.findViewById(R.id.email);
-        mPasswordView = (EditText) rootView.findViewById(R.id.password);
-        mLoginForm = rootView.findViewById(R.id.login_form);
         mProgressSpinner = rootView.findViewById(R.id.login_progress);
-        View loginButton = rootView.findViewById(R.id.email_sign_in_button);
         mGoogleSignInButton = (SignInButton) rootView.findViewById(R.id.google_sign_in_button);
-        View rosefireLoginButton = rootView.findViewById(R.id.rosefire_sign_in_button);
-        mEmailView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_NEXT) {
-                    mPasswordView.requestFocus();
-                    return true;
-                }
-                return false;
-            }
-        });
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_NULL) {
-                    login();
-                    return true;
-                }
-                return false;
-            }
-        });
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
+        rosefireLoginButton = rootView.findViewById(R.id.rosefire_sign_in_button);
+
         mGoogleSignInButton.setColorScheme(SignInButton.COLOR_LIGHT);
         mGoogleSignInButton.setSize(SignInButton.SIZE_WIDE);
         mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
@@ -99,13 +62,10 @@ public class LoginFragment extends Fragment {
         if (mLoggingIn) {
             return;
         }
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
 
         showProgress(true);
         mLoggingIn = true;
         mListener.onRosefireLogin();
-        hideKeyboard();
     }
 
 
@@ -113,62 +73,10 @@ public class LoginFragment extends Fragment {
         if (mLoggingIn) {
             return;
         }
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
 
         showProgress(true);
         mLoggingIn = true;
         mListener.onGoogleLogin();
-        hideKeyboard();
-    }
-
-
-    public void login() {
-        if (mLoggingIn) {
-            return;
-        }
-
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancelLogin = false;
-        View focusView = null;
-
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.invalid_password));
-            focusView = mPasswordView;
-            cancelLogin = true;
-        }
-
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.field_required));
-            focusView = mEmailView;
-            cancelLogin = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.invalid_email));
-            focusView = mEmailView;
-            cancelLogin = true;
-        }
-
-        if (cancelLogin) {
-            // error in login
-            focusView.requestFocus();
-        } else {
-            // show progress spinner, and start background task to login
-            showProgress(true);
-            mLoggingIn = true;
-            mListener.onLogin(email, password);
-            hideKeyboard();
-        }
-    }
-
-    private void hideKeyboard() {
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
-                Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(mEmailView.getWindowToken(), 0);
     }
 
     public void onLoginError(String message) {
@@ -185,8 +93,8 @@ public class LoginFragment extends Fragment {
 
     private void showProgress(boolean show) {
         mProgressSpinner.setVisibility(show ? View.VISIBLE : View.GONE);
-        mLoginForm.setVisibility(show ? View.GONE : View.VISIBLE);
         mGoogleSignInButton.setVisibility(show ? View.GONE : View.VISIBLE);
+        rosefireLoginButton.setVisibility(show ? View.GONE : View.VISIBLE);
     }
 
     private boolean isEmailValid(String email) {
